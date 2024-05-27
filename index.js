2153af4ab5c0e7ff9935512eee2a9a492c66ff5e
Darioooo <darionietosastre@gmail.com>
2024-05-27 19:09:04 +0200

    ponemos cors como antes

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ require("dotenv").config();
 const app = express()
 
 const port = process.env.PORT || 3000;
+const host = process.env.HOST || process.env.FRONT_URL_VERCEL;
 
-const corsOptions = {
-    origin: ['http://localhost:4200', 'https://paradise-films-frontend-angular-js.vercel.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204
-  };
+var corsOptions = {
+  origin: host,
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 
 app.use(express.json())
